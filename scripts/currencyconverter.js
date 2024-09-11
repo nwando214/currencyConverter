71dd30515c059a3e3472efd32a7f7fa88c45ce42
nwando214 <nwandoeliogu2005@gmail.com>
2024-09-11 14:48:10 +0100

    the first version

---
+++ scripts/currencyconverter.js
@@ -0,0 +1,25 @@
+import convertToDollar from "./converttodollars.js";
+import convertToEuro from "./converttoeuros.js";
+import convertToPounds from "./converttopounds.js";
+
+let selectCurrency= document.getElementById ("select-currency");
+let currencyInput= document.getElementById("currencyInput");
+let changeCurrencyBtn = document.getElementById ("changeCurrencyBtn");
+let outputDiv = document.getElementById ("output");
+let naira;
+
+changeCurrencyBtn.addEventListener("click", () => {
+    let selectedCurrency = selectCurrency.value;
+    let currency = currencyInput.value;
+
+    if (selectedCurrency === 'usd') {
+      naira = convertToDollar (currency) 
+      outputDiv.innerHTML = `$${currency} = ₦${naira}`;
+    } else if (selectedCurrency === 'euro'){
+        naira= convertToEuro (currency)
+        outputDiv.innerHTML = ` €${currency} = ₦${naira}`;
+    } else if (selectedCurrency === 'pounds'){
+        naira = convertToPounds (currency);
+        outputDiv.innerHTML = `£${currency} = ₦${naira}`;
+    }
+})
